b6c298cfab49cf460571cda86e26bba75b513e43
lzp885387491 <885387491@qq.com>
2022-06-03 15:25:27 +0800

    看了一下线上的代码

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import './common.scss'
 Vue.use(ElementUI);
 Vue.use(new VueSocketIO({
   debug: false,
+  // connection: 'http://8.131.89.181:7001/',    // 这个地址是 浩爽的 服务端地址
   connection: 'http://192.168.0.121:7001/',    // 这个地址是 浩爽的 服务端地址
   // connection: 'http://192.168.0.120:7001/',    //这个地址是 尚宇豪 服务端地址
 }))
--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
     proxy: {
       '/api': {
         // target: 'http://8.131.89.181:7001/',// 线上 服务端域名地址
-        // target: "http://192.168.0.120:7001",// 尚宇豪 服务端域名地址
         target: "http://192.168.0.121:7001",// 魏浩爽 服务端域名地址
+        // target: "http://192.168.0.120:7001",// 尚宇豪 服务端域名地址
         ws: true,
         changeOrigin: true,
         pathRewrite: { "^/api": '' }
